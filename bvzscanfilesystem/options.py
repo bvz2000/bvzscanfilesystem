56d6878a0fc888b5805d046d708c54fb8d91fe36
bvz <bvz@c381.com>
2023-02-20 23:16:09 -0800

    Added storage of UID and GID. Added None as a legal value for regexes.

--- bvzscanfilesystem/options.py
+++ bvzscanfilesystem/options.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import os
 
 
 @dataclass
@@ -7,8 +8,10 @@ class Options:
     skip_hidden_files: bool = False
     skip_hidden_dirs: bool = False
     skip_zero_len: bool = True
-    incl_dir_regexes: list = None
-    excl_dir_regexes: list = None
-    incl_file_regexes: list = None
-    excl_file_regexes: list = None
+    incl_dir_regexes: (None, list) = None
+    excl_dir_regexes: (None, list) = None
+    incl_file_regexes: (None, list) = None
+    excl_file_regexes: (None, list) = None
     report_frequency: int = 10
+    uid: int = os.getuid()
+    gid: int = os.getgid()
